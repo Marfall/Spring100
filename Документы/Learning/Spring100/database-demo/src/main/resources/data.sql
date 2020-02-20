b765a1758dcc4961f98f8ddd505d646c76c77a74
Marfall <marinefalling@gmail.com>
2020-02-20 16:21:30 +0300

    before jpa

---
+++ Документы/Learning/Spring100/database-demo/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO person (id, name, location, birth_date)
+VALUES(10001, 'Ranga', 'Hyderabad',sysdate());
+INSERT INTO person (id, name, location, birth_date)
+VALUES(10002, 'James', 'New York',sysdate());
+INSERT INTO person (id, name, location, birth_date)
+VALUES(10003, 'Pieter', 'Amsterdam',sysdate());
